739a0194a0d97499fde9d000c299da0fd5affbbe
Yeeun Min <elmamin24@gmail.com>
2018-05-27 23:45:12 -0700

    Yeeeeeeeeun's work

--- src/main/java/apcs/tamagochi/entity/Pet.java
+++ src/main/java/apcs/tamagochi/entity/Pet.java
@@ -7,15 +7,22 @@ import java.awt.*;
 public class Pet extends GameObject {
     //health
     private int health;
-    private int maxHealth;
+    private int maxHealth; //100
 
-    //hunger
-    private int hunger;
-    private int macHunger;
+    //full
+    private int full;
+    private int maxFull; //100
 
     //enjoyment
     private int enjoyment;
-    private int maxEnjoyment;
+    private int maxEnjoyment; //100
+
+    //expperience. amount of exp to level-up increases by *1.5 every level.
+    //
+    private int exp;
+    private int lvl; // peneggdf lv1 ~ lv3, peneggshake lv4, penbabydf lv5 ~ lv9, penadol lv10 ~
+
+
 
 
     @Override
